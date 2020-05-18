c9d7c458d0622fd74d98d1e1d07b16c0bf7d9bda
Ethan <ethan@etekweb.net>
2020-05-17 21:29:43 -0500

    moved about section to separate component

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,21 @@
 <template>
   <div id="app">
     <TheHeader />
-    <div class="about">
-      <h1>Hello, world!</h1>
-      <p>
-        My name is Ethan Matzdorf. 
-        I was born and raised in Cloquet, MN and am currently pursuing a B.S. in Computer Science from Michigan Technological University.
-      </p>
-      <p>
-        From a young age, I have taken great interest in technology in various forms, exploring and learning about all of the many possibilities they contain.<br>
-        I experimented with rudimentary web design starting at age seven, and in sixth grade began learning the basics of HTML, CSS, PHP and MySQL.<br>
-        From there, I took initiative in developing web projects for my community that are in production use today: a registration utility for a middle school enrichment class program, and a trail map system at a local park.
-      </p>
-      <p>
-        In college, I have been working for 24G, a digital experience agency, as a JavaScript developer.<br>
-        <!-- TODO - add link to section when added -->
-        <span class="link">See what I did there</span>.
-      </p>
-    </div>
+    <AboutSection />
     <SocialsAndResume />
   </div>
 </template>
 
 <script>
 import TheHeader from '@/components/TheHeader.vue';
+import AboutSection from '@/components/AboutSection.vue';
 import SocialsAndResume from '@/components/SocialsAndResume.vue';
 
 export default {
   name: 'App',
   components: {
     TheHeader,
+    AboutSection,
     SocialsAndResume
   }
 };
@@ -55,11 +41,6 @@ h1 {
 }
 p {
   font-size: 22px;
-}
-.about {
-  margin-top: 55px;
-  margin-left: 9vw;
-  max-width: 57vw;
 }
 .link {
   color: rgb(0, 0, 200);
---
+++ src/components/AboutSection.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h1>Hello, world!</h1>
+    <p>
+      My name is Ethan Matzdorf. 
+      I was born and raised in Cloquet, MN and am currently pursuing a B.S. in Computer Science from Michigan Technological University.
+    </p>
+    <p>
+      From a young age, I have taken great interest in technology in various forms, exploring and learning about all of the many possibilities they contain.<br>
+      I experimented with rudimentary web design starting at age seven, and in sixth grade began learning the basics of HTML, CSS, PHP and MySQL.<br>
+      From there, I took initiative in developing web projects for my community that are in production use today: a registration utility for a middle school enrichment class program, and a trail map system at a local park.
+    </p>
+    <p>
+      In college, I have been working for 24G, a digital experience agency, as a JavaScript developer.<br>
+      <!-- TODO - add link to section when added -->
+      <span class="link">See what I did there</span>.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.about {
+  margin-top: 55px;
+  margin-left: 9vw;
+  max-width: 57vw;
+}
+</style>
