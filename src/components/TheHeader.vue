8ea6b3fac142f68ab90400b96065fabc22cc0d92
Ethan <ethan@etekweb.net>
2020-05-17 19:35:36 -0500

    added header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+    Ethan Matzdorf
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.header {
+  width: 100%;
+  min-height: 400px;
+  height: 50vh;
+  background-image: url('../assets/banner.jpg');
+  background-position-y: bottom;
+  background-size: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font: 70px/96px 'Open Sans';
+  letter-spacing: 0px;
+  color: #FFFFFF;
+}
+</style>
