acaf6655cec006db9f72a6ac6a5638a7d1132e80
Ethan <ethan@etekweb.net>
2020-05-17 21:05:37 -0500

    added about section, social / resume layout

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <template>
   <div id="app">
     <TheHeader />
+    <div class="about">
+      <h1>Hello, world!</h1>
+      <p>
+        My name is Ethan Matzdorf. 
+        I was born and raised in Cloquet, MN and am currently pursuing a B.S. in Computer Science from Michigan Technological University.
+      </p>
+      <p>
+        From a young age, I have taken great interest in technology in various forms, exploring and learning about all of the many possibilities they contain.<br>
+        I experimented with rudimentary web design starting at age seven, and in sixth grade began learning the basics of HTML, CSS, PHP and MySQL.<br>
+        From there, I took initiative in developing web projects for my community that are in production use today: a registration utility for a middle school enrichment class program, and a trail map system at a local park.
+      </p>
+      <p>
+        In college, I have been working for 24G, a digital experience agency, as a JavaScript developer. 
+        <span class="link">See what I did there</span>.
+      </p>
+    </div>
+    <SocialsAndResume />
   </div>
 </template>
 
 <script>
 import TheHeader from '@/components/TheHeader.vue';
+import SocialsAndResume from '@/components/SocialsAndResume.vue';
 
 export default {
   name: 'App',
   components: {
     TheHeader,
+    SocialsAndResume
   }
 };
 </script>
@@ -23,6 +42,43 @@ html, body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   font-family: 'Open Sans';
+  font-size: 28px;
   /* font-family: 'Roboto Slab'; */
+}
+h1 {
+  display: block;
+  font-size: 48px;
+  margin: 30px 0;
+  color: black;
+  font-weight: normal;
+}
+p {
+  font-size: 28px;
+}
+.about {
+  margin-top: 55px;
+  margin-left: 16vw;
+  max-width: 57vw;
+}
+.link {
+  color: rgb(0, 0, 200);
+  text-decoration: underline;
+  font-weight: bolder;
+  cursor: pointer;
+}
+.minor.btn {
+  padding: 14px 36px;
+  border: 1px solid #707070;
+  background-color: white;
+  border-radius: 26px;
+  text-transform: uppercase;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font: 26px/33px "Open Sans";
+  font-weight: 100;
+  color: #595959;
+  letter-spacing: 1.56px;
 }
 </style>
---
+++ src/components/SocialsAndResume.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="social">
+    <h1>Find me on</h1>
+    <img class="icon" src="@/assets/github.png">
+    <img class="icon" src="@/assets/linkedin.png">
+    <div class="divider" />
+    <div class="minor btn">Printable Resume</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+.social {
+  margin: 57px 0 47px 0;
+  width: 100%;
+  height: 160px;
+  background-color: #F0F2F2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.icon {
+  width: 88px;
+  height: 88px;
+  margin: 0 0 0 25px;
+}
+.divider {
+  width: 1px;
+  height: 75px;
+  background-color: black;
+  margin: 0 40px;
+}
+</style>
